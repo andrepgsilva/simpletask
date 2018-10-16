c3c2177678d3cc86e73766e5eb77501131fa6f29
Andre Luiz Pergentino da Silva <andrepgsilva@outlook.com>
2018-10-16 17:19:09 +0100

    Configuration file added

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'db' => [
+        'type' => 'mysql',
+        'host' => 'localhost',
+        'name' => 'simpletask',
+        'username' => '',
+        'password' => '',
+        'options' => [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    ]
+];
