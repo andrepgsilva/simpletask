fe6044239477c2bd6bc55e2ba31538bd11364dd9
Andre Luiz Pergentino da Silva <andrepgsilva@outlook.com>
2018-10-20 22:51:23 +0100

    Update nav.php

--- Views/Partials/nav.php
+++ Views/Partials/nav.php
@@ -1,7 +1,7 @@
 <nav>
     <ul>
         <li><a href="/simpletask/">Home</a></li>
-        <li><a href="task/show">Tasks</a></li>
+        <li><a href="/simpletask/task/show">Tasks</a></li>
         <li><a href="/simpletask/contact">Contact</a></li>
     </ul>
 </nav>
